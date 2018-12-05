a8e8b8e271fc5c7cfb28379341391aa7e63f0842
Theresa Piersall <tmp2109@rit.edu.com>
2018-12-04 22:48:24 -0500

    progress proj 3

--- project3/medCanvas.js
+++ project3/medCanvas.js
@@ -2,8 +2,29 @@ const numCols = 28;//num of times
 const numRows = 19;//num of times
 const cellSize = 25;//px
 const numCells = numCols * numRows;
+let opac = ("rgba(0, 0, 0, 0.2)");
 let pixelGrid = document.querySelector("#pixelGrid");
 let color = $("#colorChooser").val(); //
+let medGrid = 
+  [ 13, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 8, 3, 3, 3, 3, 13, 3, 3, 3, 3, 3, 3, 3, 13, 3, 3, 3,
+    13, 13, 13, 9, 13, 8, 8, 9, 5, 5, 5, 9, 8, 9, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
+    13, 3, 8, 8, 9, 8, 8, 8, 9, 5, 5, 9, 8, 8, 8, 4, 13, 3, 3, 3, 4, 4, 4, 3, 3, 13, 3, 3,
+    13, 3, 8, 8, 8, 8, 11, 5, 9, 5, 5, 11, 5, 8, 8, 4, 3, 13, 3, 3, 3, 3, 4, 4, 4, 13, 3, 3,
+    13, 3, 8, 8, 8, 8, 11, 11, 9, 5, 5, 11, 11, 8, 10, 8, 3, 13, 3, 3, 3, 3, 3, 3, 3, 3, 13, 3,
+    13, 3, 8, 8, 10, 8, 8, 9, 5, 5, 5, 5, 9, 8, 10, 8, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 13, 3,
+    13, 13, 8, 8, 10, 8, 8, 5, 5, 11, 11, 11, 5, 8, 10, 8, 13, 2, 2, 2, 13, 2, 13, 13, 13, 13, 2, 2,
+    13, 3, 8, 8, 10, 8, 9, 5, 5, 5, 11, 11, 5, 5, 10, 8, 8, 4, 2, 3, 3, 3, 4, 4, 4, 4, 4, 2,
+    2, 3, 8, 8, 10, 10, 9, 5, 5, 5, 5, 5, 5, 5, 3, 9, 8, 8, 2, 3, 3, 3, 3, 3, 3, 4, 4, 3,
+    2, 3, 8, 8, 10, 10, 6, 6, 5, 5, 12, 12, 5, 6, 3, 3, 3, 9, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+    13, 3, 9, 8, 8, 10, 5, 6, 6, 6, 6, 6, 6, 5, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+    2, 3, 9, 8, 8, 10, 5, 5, 5, 5, 5, 5, 5, 7, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+    2, 3, 3, 9, 8, 9, 5, 7, 7, 7, 7, 7, 7, 5, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3,
+    2, 2, 2, 5, 8, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 3, 3, 5, 5, 5, 5, 5, 5, 5, 6, 5, 5, 5, 1, 3, 4, 4, 4, 4, 2, 3, 3, 4, 4, 4, 4, 4,
+    2, 3, 1, 5, 5, 6, 5, 5, 5, 6, 5, 6, 5, 5, 1, 1, 3, 3, 4, 4, 4, 2, 3, 3, 3, 3, 4, 4,
+    2, 1, 1, 1, 5, 6, 5, 5, 5, 5, 5, 6, 5, 5, 1, 1, 1, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 4,
+    2, 1, 1, 1, 1, 6, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3,
+    2, 3, 1, 1, 1, 1, 5, 5, 5, 1, 1, 5, 5, 5, 1, 1, 1, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3 ];
 
 /*let rSlider = document.getElementById("redslide");
 let rOutput = document.getElementById("rtest");
@@ -28,12 +49,13 @@ bOutput.innerHTML = bSlider.value;
 bSlider.oninput = function() {
     bOutput.innerHTML = this.value;
 }*/
+$("#swatch").css("background-color", color);
 
 pixelGrid.style.gridTemplateRows = `repeat(${numRows}, ${cellSize}px)`; //JS doesnt like dashes so you have to do camel case
 pixelGrid.style.gridTemplateColumns = `repeat(${numCols},${cellSize}px)`; //have to tell it to repeat # and then tell it what type it is so in this case px
 
 for (let i = 0; i < numCells; i++){
-    $("#pixelGrid").append('<span class="cell"></span>');
+    $("#pixelGrid").append('<span class="cell">' + medGrid[i] + '</span>');
 }
 
 $("#colorChooser").change(function() {
@@ -45,8 +67,10 @@ $("#colorChooser").change(function() {
 $(".cell").mousedown(function(){
     event.preventDefault();
     $(this).css("background-color", color);
+    $(this).css("color", opac);
     $(".cell").mouseover(function(){
         $(this).css("background-color", color);
+        $(this).css("color", opac);
     });
 });
 
@@ -56,4 +80,5 @@ $(document).mouseup(function(){
         
 $("#reset").click(function(){
     $(".cell").css("background-color", "#ccc");
+    $(".cell").css("color", "rgba(0, 0, 0)");
 });
--- project3/project3.js
+++ project3/project3.js
@@ -3,26 +3,46 @@ const numRows = 15;//num of times
 const cellSize = 30;//px
 const numCells = numCols * numRows;
 let pixelGrid = document.querySelector("#pixelGrid");
+let opac = ("rgba(0, 0, 0, 0.2)");
 let color = $("#colorChooser").val(); //
+let easyGrid = 
+  [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 6, 6, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 6, 6, 6, 6, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 5, 5, 8, 5, 6, 7, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 9, 9, 9, 9, 5, 5, 6, 6, 3, 3, 3, 3, 3,
+    3, 3, 3, 3, 3, 3, 3, 3, 9, 9, 9, 9, 9, 6, 6, 6, 3, 3, 3, 3, 3, 3,
+    3, 3, 3, 3, 3, 3, 3, 10, 10, 10, 9, 9, 11, 11, 5, 5, 3, 3, 3, 3, 3, 3,
+    3, 3, 3, 3, 3, 9, 9, 9, 9, 9, 9, 11, 11, 11, 5, 3, 3, 3, 3, 3, 3, 3,
+    3, 3, 3, 3, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 3, 3, 3, 3, 3, 3, 3, 3,
+    2, 2, 10, 10, 10, 2, 2, 2, 11, 11, 11, 11, 11, 2, 2, 2, 2, 4, 4, 2, 12, 12,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 2, 7, 2, 2, 2, 2, 2, 4, 4, 12, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 2, 2, 7, 2, 2, 2, 12, 12, 12, 12, 2, 2,
+    1, 1, 4, 4, 1, 1, 1, 1, 1, 1, 7, 1, 12, 7, 7, 12, 4, 4, 1, 1, 1, 1,
+    1, 12, 4, 4, 12, 12, 12, 12, 12, 7, 7, 12, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, ];
 
 pixelGrid.style.gridTemplateRows = `repeat(${numRows}, ${cellSize}px)`; //JS doesnt like dashes so you have to do camel case
 pixelGrid.style.gridTemplateColumns = `repeat(${numCols},${cellSize}px)`; //have to tell it to repeat # and then tell it what type it is so in this case px
 
 for (let i = 0; i < numCells; i++){
-    $("#pixelGrid").append('<span class="cell"></span>');
+    $("#pixelGrid").append('<span class="cell">' + easyGrid[i] + '</span>');
 }
 
 $("#colorChooser").change(function() {
     color = $(this).val();
     console.log(color);
     $("#swatch").css("background-color", color);
+    
 });
 
 $(".cell").mousedown(function(){
     event.preventDefault();
     $(this).css("background-color", color);
+    $(this).css("color", opac);
     $(".cell").mouseover(function(){
         $(this).css("background-color", color);
+        $(this).css("color", opac);
     });
 });
 
@@ -32,4 +52,5 @@ $(document).mouseup(function(){
         
 $("#reset").click(function(){
     $(".cell").css("background-color", "#ccc");
+    $(".cell").css("color", "rgba(0, 0, 0)");
 });
